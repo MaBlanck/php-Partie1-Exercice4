2882d45ecb9e176ead289005515fec2c1e3c66ef
Max <maxblanck@outlook.fr>
2020-12-09 09:25:13 +0100

    php Partie 1  Exercice 4

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
+<?php
+$string = 'Hello You';
+$int = 30;
+$float = 18.25;
+$bool = true;
+?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Exercice 4 Partie 1</title>
 </head>
+
 <body>
     <h1>Exercice 4 Partie 1</h1>
-
-    <?php 
-    $string = 'Hello You';
-    $int = 30;
-    $float = 18.25;
-    $bool = true;
-    echo ($string . ' ' . $int . ' ' . $float . ' ' . $bool);
-    ?>
+    <p><?= ($string . ' ' . $int . ' ' . $float . ' ' . $bool); ?></p>
 
 </body>
 </html>
